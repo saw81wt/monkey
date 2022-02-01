19c64dac74882d7ba7b4f5ec45c90a4823ee4bac
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-01 21:50:32 +0900

    replじっそう

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main () {
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"my_monkey/repl"
+)
 
+func main () {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to ypye in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
