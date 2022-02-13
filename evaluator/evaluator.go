b42f18c6a1885aa1922f376084755612ec799db1
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-13 17:05:26 +0900

    整数比較は真偽値比較よりはやい

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -85,6 +85,10 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBooleanObject(left != right)
 	default:
 		return NULL
 	}
