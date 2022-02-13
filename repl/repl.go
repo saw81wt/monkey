925ff1467d718daf808f4217b51f6b4d19b84e70
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-13 16:20:01 +0900

    relpこうしん

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"my_monkey/lexer"
 	"my_monkey/parser"
+	"my_monkey/evaluator"
 )
 
 const PROMPT = ">> "
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
