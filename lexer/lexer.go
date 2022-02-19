42a5b269f263325ebfce2d5e20b17e610a26f405
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-19 19:47:00 +0900

    配列

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -42,6 +42,10 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
 	case ',':
 		tok = newToken(token.COMMA, l.ch)
 	case '+':
@@ -65,7 +69,6 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LT, l.ch)
 	case '>':
 		tok = newToken(token.GT, l.ch)
-
 	case '"':
 		tok.Type = token.STRING
 		tok.Literal = l.readString()
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -28,6 +28,7 @@ func TestNextToken(t *testing.T) {
 	10 != 9
 	"foobar"
 	"foo bar"
+	[1, 2];
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -105,6 +106,12 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "9"},
 		{token.STRING, "foobar"},
 		{token.STRING, "foo bar"},
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
