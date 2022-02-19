3824119d0d305eda4e32a4acc0817b5818ee1f06
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-19 17:32:37 +0900

    string literal

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -26,6 +26,8 @@ func TestNextToken(t *testing.T) {
 
 	10 == 10
 	10 != 9
+	"foobar"
+	"foo bar"
 	`
 	tests := []struct {
 		expectedType    token.TokenType
@@ -101,6 +103,8 @@ func TestNextToken(t *testing.T) {
 		{token.INT, "10"},
 		{token.NOT_EQ, "!="},
 		{token.INT, "9"},
+		{token.STRING, "foobar"},
+		{token.STRING, "foo bar"},
 		{token.EOF, ""},
 	}
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -14,6 +14,7 @@ const (
 	// 識別子 + リテラル
 	IDENT = "IDENT"
 	INT = "INT"
+	STRING  = "STRING"
 
 	ASSIGN = "="
 	PLUS = "+"
