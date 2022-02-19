4977bae5cc909af7f6df3902f489e73026006459
Sotaro Maehara <s.maehara81@icloud.com>
2022-02-20 00:30:19 +0900

    辞書の追加

--- object/object.go
+++ object/object.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"bytes"
 	"strings"
+	"hash/fnv"
 
 	"my_monkey/ast"
 )
@@ -24,11 +25,27 @@ const (
 	ERROR_OBJ = "ERROR"
 
 	BUILTIN_OBJ = "BUILTIN"
+
+	HASH_OBJ = "HASH"
 )
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
+type HashKey struct {
+	Type ObjectType
+	Value uint64
+}
+
+type HashPair struct {
+	Key Object
+	Value Object
 }
 
 type Integer struct {
@@ -37,6 +54,9 @@ type Integer struct {
 
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
 
 type Boolean struct {
 	Value bool
@@ -44,6 +64,17 @@ type Boolean struct {
 
 func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
 
 type String struct {
 	Value string
@@ -51,6 +82,11 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_ONJ }
 func (s *String) Inspect() string { return s.Value}
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
+}
 
 type Null struct {}
 
@@ -119,6 +155,28 @@ func (ao *Array) Inspect() string {
 	out.WriteString("[")
 	out.WriteString(strings.Join(elements, ", "))
 	out.WriteString("]")
+
+	return out.String()
+}
+
+
+type Hash struct {
+	Pairs map[HashKey]HashPair
+}
+
+func (h *Hash) Type() ObjectType { return HASH_OBJ }
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s",
+			pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
 
 	return out.String()
 }
